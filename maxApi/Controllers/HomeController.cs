564326718064e847ddbf888e13feb7f5569988cc
assafsabag <sabag.assaf@gmail.com>
2020-12-10 15:11:10 +0200

    using keycloak

--- maxApi/Controllers/HomeController.cs
+++ maxApi/Controllers/HomeController.cs
@@ -3,9 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.AspNetCore.Authorization;
 namespace maxApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class HomeController : Controller
--- maxApi/Startup.cs
+++ maxApi/Startup.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace maxApi
 {
@@ -25,21 +26,46 @@ namespace maxApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(
+                   options =>
+                   {
+                       options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                       options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                   }).AddJwtBearer(o =>
+                   {
+                       //for example https://localhost:8080/auth/realms/Master,
+                       o.Authority = Configuration["Jwt:Authority"];
+                       o.Audience = Configuration["Jwt:Audience"];
+                       o.Events = new JwtBearerEvents()
+                       {
+                           OnAuthenticationFailed = c =>
+                           {
+                               c.NoResult();
+
+                               c.Response.StatusCode = 500;
+                               c.Response.ContentType = "text/plain";
+
+                               return c.Response.WriteAsync(c.Exception.ToString());
+
+                           }
+                       };
+                   });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        { 
+        {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
